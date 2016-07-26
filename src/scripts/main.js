505f72395081e009ae0c7aa4c53b590d9551e13e
Snigur Dmitry <snidima@mail.ru>
2016-07-26 22:14:26 +0700

    add react-router

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,8 @@
 import React from 'react'
-
+import { Router, Route, Link, browserHistory } from 'react-router'
 
 //React.render(
 //    <h1>test</h1>,
 //    document.getElementById('test')
 //);
 
-let obj = {
-    el: 'qwe',
-    arr: [
-        {
-            lol: 1,
-            lol2: 2
-        },
-        {
-            lol:5,
-            lol2:80
-        }
-    ]
-};
-
-localStorage.setItem('qwe', JSON.stringify(obj));
-console.log(JSON.parse(localStorage.getItem('qwe')));
