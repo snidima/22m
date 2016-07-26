4ea3a4a8215e03a414ecda8aeccb1c64834d6bc1
Snigur Dmitry <snidima@mail.ru>
2016-07-26 16:01:20 +0700

    header - ok, slider - ok, item - ok, submenu - ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-console.log(React);
+
 //React.render(
 //    <h1>test</h1>,
 //    document.getElementById('test')
 //);
+
+let obj = {
+    el: 'qwe',
+    arr: [
+        {
+            lol: 1,
+            lol2: 2
+        },
+        {
+            lol:5,
+            lol2:80
+        }
+    ]
+};
+
+localStorage.setItem('qwe', JSON.stringify(obj));
+console.log(JSON.parse(localStorage.getItem('qwe')));
