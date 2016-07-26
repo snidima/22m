8e6ed4479a5097b614c1922568068509a10cde2f
Snigur Dmitry <snidima@mail.ru>
2016-07-27 03:40:23 +0700

    add popup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,33 @@
 import React from 'react'
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, Link, browserHistory, hashHistory } from 'react-router'
 
 //React.render(
 //    <h1>test</h1>,
 //    document.getElementById('test')
 //);
 
+const Home = React.createClass({
+    render(){
+        return(
+            <h1>Home</h1>
+        )
+    }
+});
+
+const Tshirts = React.createClass({
+    render(){
+        return(
+            <h1>Tshirts</h1>
+        )
+    }
+});
+
+
+
+React.render(
+    <Router history={browserHistory}>
+        <Route path="/" component={Home} />
+        <Route path="/tshirts" component={Tshirts} />
+    </Router>,
+    document.getElementById('page')
+);
